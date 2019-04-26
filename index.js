c5e3dcd92b8798e4bc06a01a4e2dc76f9898fd5e
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-26 16:35:03 +0700

    Install chalk and chalk animation, edit on index.html and package.json

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hello");
+const chalk = require("chalk");
+const chalkAnimmation = require("chalk-animation");
+console.log(chalk.blue("Hello World!"));
+chalkAnimmation.glitch("Hello World!");
